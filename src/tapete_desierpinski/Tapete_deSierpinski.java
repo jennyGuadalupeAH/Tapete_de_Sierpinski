597bc9cbd1afd8925eb3e9f9024dc932fae69d7c
Jennifer Guadalupe <jennyangeleshernandez310@gmail.com>
2024-09-13 11:08:26 -0600

    ADD: Ajustes y delete comentarios no necesarios

--- src/tapete_desierpinski/Tapete_deSierpinski.java
+++ src/tapete_desierpinski/Tapete_deSierpinski.java
@@ -16,35 +16,35 @@ import java.awt.event.MouseEvent;
 
 public class Tapete_deSierpinski extends JPanel {
 
-    // Tamaño inicial del cuadrado y margen
+    
     private final int initialSize = 400;
     private final int border = 20;
 
-    // Título para mostrar en el panel
+   
     private final String title = "Tapete de Sierpinski";
 
     public Tapete_deSierpinski() {
-        // Configurar el panel para detectar clics del ratón
+      
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                repaint(); // Redibuja el panel cuando se presiona el ratón
+                repaint(); 
             }
         });
     }
 
-  
+
 
     public static void main(String[] args) {
-        // Crear la ventana principal
-        JFrame frame = new JFrame("Sierpinski Carpet");
+      
+        JFrame frame = new JFrame("Tapete de Sierpinski");
         Tapete_deSierpinski panel = new Tapete_deSierpinski();
 
-        // Configuración de la ventana
+    
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
-        frame.setSize(500, 500); // Tamaño de la ventana
-        frame.setLocationRelativeTo(null); // Centrar la ventana en la pantalla
+        frame.setSize(500, 500); 
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
